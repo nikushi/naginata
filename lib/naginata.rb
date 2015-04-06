dc3cb62000f072545012150b5e5810189751657d
Nobuhiro Nikushi <deneb.ge@gmail.com>
2015-04-06 10:38:16 +0900

    Fix uninitialized constant Naginata (NameError)
    This raises when require external_command before define Naginata
    namespace.

--- lib/naginata.rb
+++ lib/naginata.rb
@@ -1,16 +1,16 @@
-require "naginata/command/external_command"
-require "naginata/configuration"
-require "naginata/configuration/filter"
-require "naginata/configuration/nagios_server"
-require "naginata/configuration/service"
-require "naginata/runner"
-require "naginata/status"
-require "naginata/ui"
-require "naginata/version"
-
 require "nagios_analyzer/section_decorator"
 
 module Naginata
+
+  require "naginata/command/external_command"
+  require "naginata/configuration"
+  require "naginata/configuration/filter"
+  require "naginata/configuration/nagios_server"
+  require "naginata/configuration/service"
+  require "naginata/runner"
+  require "naginata/status"
+  require "naginata/ui"
+  require "naginata/version"
 
   class NaginatafileNotFound < StandardError; end
 
