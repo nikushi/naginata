4b13b1d68b45b78a01f48b49ce5fd0f7c0f1540c
Nobuhiro Nikushi <deneb.ge@gmail.com>
2015-08-05 12:33:53 +0900

    Fix typo

--- lib/naginata/shared_helpers.rb
+++ lib/naginata/shared_helpers.rb
@@ -7,7 +7,7 @@ module Naginata
         ~/.naginata/Naginatafile
         /etc/naginata/Naginatafile
       ).map{ |path| File.expand_path(path) }.find { |path| File.file?(path) }
-      raise NaginatafileNotFound, "Could not locate Gemfile" unless file
+      raise NaginatafileNotFound, "Could not locate Naginatafile" unless file
       return file
     end
 
